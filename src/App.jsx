f10ac26319dccfb2066ba57e765daade60ed229c
mrs <mrs@localhost.localdomain>
2025-01-03 14:05:14 +0600

    learning step1

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+
+import './App.css'
+import Nav from './comp/Nav'
+import Sec from './comp/Sec'
+
+
+
+
+function App() {
+  
+
+  return (
+
+
+
+    <>
+    <Nav/>
+
+
+<Sec/>
+
+      
+
+
+
+    </>
+
+
+
+  )
+}
+
+
+
+export default App
---
+++ src/comp/Nav.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import './Css.css'
+function Nav(){
+
+    return(
+
+        <nav>
+        <div>
+          <span>Figma Developer</span>
+        </div>
+        <div class="hyp">
+          <ul class="un">
+            <li>Compare APIs</li>
+            <li class="pa1">
+              Plugin APIs <img src="arr.png" height="12px" width="12px" alt="" />
+              <div class="ch1">
+                <ul>
+                  <li>Guide</li>
+                  <li>Api Reference</li>
+                  <li>Updates</li>
+                </ul>
+              </div>
+            </li>
+            <li class="pa">
+              Widget APIs<img src="arr.png" height="12px" width="12px" alt="" />
+            </li>
+            <li>REST APIs</li>
+            <li>Code connect</li>
+            <li>Embeds</li>
+            <li>SCIM</li>
+            <li>My Apps</li>
+            <li>login</li>
+          </ul>
+          <button type="button" class="btn">Sign up</button>
+        </div>
+      </nav>
+  
+
+
+    )
+}
+
+export default Nav
